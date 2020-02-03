f138b4f621c08cfa3dd18197e3c32bf55fb2dd6d
Michael Harrington <michael.harrington@zoom.us>
2020-02-03 14:28:36 -0700

    works

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ app.get('/', (req, res) => {
 
     if (req.query.code) {
 
-        let url = 'https://zoom.us/oauth/authorize?response_type=code&client_id=' + process.env.clientID + '&redirect_uri=' + process.env.redirectURL;
+        // res.send(req.query.code)
+
+        // let url = 'https://zoom.us/oauth/authorize?response_type=code&client_id=' + process.env.clientID + '&redirect_uri=' + process.env.redirectURL;
 
         // // Step 3: 
         // // Request an Access Token
@@ -28,7 +30,57 @@ app.get('/', (req, res) => {
             
         //     // convert response to JSON 
         //     body = JSON.parse(body)
+
+        //     console.log(body.access_token);
+        //     // res.send(body.access_token)
+
         // }).auth(process.env.clientID, process.env.clientSecret)
+
+        let url = 'https://zoom.us/oauth/token?grant_type=authorization_code&code=' + req.query.code + '&redirect_uri=' + process.env.redirectURL;
+
+        // STEP 3
+        // we need to exchange the code for a oauth token
+        request.post(url, function (error, response, body) {
+
+            // the response should be a JSON payload
+            body = JSON.parse(body);
+            //html for displaying response in the browser
+            //Prettify the JSON format using pre tag and JSON.stringify
+
+            //print access_token in the browser
+            console.log(`access_token: ${body.access_token}`);
+            console.log(`refresh_token: ${body.refresh_token}`);
+
+            if (body.access_token) {
+
+                // STEP 4
+                // we can now use the access token to make API calls
+                request.get('https://api.zoom.us/v2/users/me', function (error, response, body) {
+                    if (error) {
+                        console.log('Error in API ', error)
+                    } else {
+                        body = JSON.parse(body);
+                        //display response in console
+                        console.log('API call ', body);
+                        //display response in browser
+                        var result = '<code><pre>' + JSON.stringify(body, null, 2) + '</pre></code>'
+                        res.send(result);
+
+                    }
+
+                }).auth(null, null, true, body.access_token);
+
+            } else {
+                //handle error, something went wrong
+            }
+
+        }).auth(process.env.clientID, process.env.clientSecret);
+
+        return;
+
+
+
+
 
     }
 
